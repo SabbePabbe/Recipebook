a7aa695bc541bdaa4e0ae535735cacf8f675988f
August Lennar <lennar@student.chalmers.se>
2018-02-22 11:54:04 +0100

    Added classes for Rating (base + @IdClass) and Category (enum)

---
+++ src/core/RatingPK.java
@@ -0,0 +1,31 @@
+package core;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@EqualsAndHashCode
+public class RatingPK implements Serializable {
+
+    @Getter
+    private String userId;
+
+    @Getter
+    private String recipeId;
+
+
+    public RatingPK(String userId, String recipeId) {
+        this.userId = userId;
+        this.recipeId = recipeId;
+    }
+
+
+    @Override
+    public String toString() {
+        return "RatingPK{" +
+                "userId='" + userId + '\'' +
+                ", recipeId='" + recipeId + '\'' +
+                '}';
+    }
+}
